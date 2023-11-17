0f419a709844b4ab72d0e9eabb9d55a6040f44f3
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-17 09:46:28 -0700

    Created Authenticate component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
 
 function App() {
 
   return (
     <>
     <SignUpForm />
+    <Authenticate />
     </>
   )
 }
