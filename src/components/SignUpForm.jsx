13c5db8448f579ccc04a52367c60201fce33ca18
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-17 10:20:52 -0700

    set value of each input = to the related variable.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,22 +1,26 @@
 import React, {useState}from 'react'
 
-// const [username, setUsername] = useState("");
-// const [password, setPassword] = useState("");
-// const [error, setError] = useState(null);
-
 
 const SignUpForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    // const handleSubmit = async (event) => {
+
+    // }
+
     return(
     <>
         <h2>Sign Up!</h2>
         <form>
     <label>
-        Username: <input />
+        Username: <input value={username} onChange={(event)=> setUsername(event.target.value)}/>
     </label>
     <label>
-        Password: <input />
+        Password: <input value={password} onChange={(even)=> setPassword(event.target.value)}/>
     </label>
-    <button>Submit</button>
+    <button type='submit'>Submit</button>
     </form>
     </>
     )
