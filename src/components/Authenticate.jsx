80b16811a463338203f64a862dab2f62f9b6e218
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-17 13:05:01 -0700

    Fixed error by simply changing single quotes '' to backticks  in the Authorization header.

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -10,7 +10,7 @@ const Authenticate = ({token}) => {
                 method: 'GET',
                 headers: {
                     "Content-Type": "application/json",
-                    Authorization: 'Bearer ${token}',
+                    Authorization: `Bearer ${token}`,
                 },
             }
             );
