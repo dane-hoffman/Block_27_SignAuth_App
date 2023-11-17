8a55019a4a1e0d49580d6b0680d574827b237bcd
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-17 10:06:30 -0700

    <form> element within the body of SignUpForm component.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,12 +1,26 @@
 import React, {useState}from 'react'
 
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState(null);
+// const [username, setUsername] = useState("");
+// const [password, setPassword] = useState("");
+// const [error, setError] = useState(null);
 
 
 const SignUpForm = () => {
-    return <h2>Sign Up!</h2>
+    return(
+    <>
+        <h2>Sign Up!</h2>
+        <form>
+    <label>
+        Username: <input />
+    </label>
+    <label>
+        Password: <input />
+    </label>
+    <button>Submit</button>
+    </form>
+    </>
+    )
+
 }
 
 export default SignUpForm;
